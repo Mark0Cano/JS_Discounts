2d5f53ee69e27e15277f156350ffb8b9534efb15
Mark_GekkoGo <marko.o0kami@gmail.com>
2021-07-30 16:22:10 -0500

    La funcion esta completa

--- jsFile.js
+++ jsFile.js
@@ -1,91 +1,119 @@
-/*  ----    Formules  ----  */
+/*  ----    Prices & Discounts  ----  */
 const availableDiscounts = [15, 20, 30, 40];
 
+const prices = [2500, 3850, 5620];
 
 /*  ----  Tickets Const  ----  */
-const ticket_01 = 'GEN00105';
-const ticket_02 = 'MOM00358';
-const ticket_03 = 'DAD00225';
-const ticket_04 = 'STD00525';
-
-/*  ----  Get Elements  ----  */
-const calButton = document.getElementById('calculate');
-calButton.addEventListener('click', addDiscount);
-
-function addDiscount()
-{
-    
-}
-
+const ticket = [
+  'GEN00105',
+  'MOM00358',
+  'DAD00225',
+  'STD00525'
+];
 
 /*  ----  Add Article Function  ----  */
 const botoncitoAñadir = document.getElementById('addItem');
-botoncitoAñadir.addEventListener('click', addItemDiscount);
+botoncitoAñadir.addEventListener('click', summArticles);
 
-let articulito = 
+/*  ----  Article selection inputs  ----  */
+const articulito = 
 [
     document.getElementById('tDis01'), 
     document.getElementById('tDis02'), 
-    document.getElementById('tDis02')
+    document.getElementById('tDis03')
 ];
 
+/*  ----  Discounts selection inputs  ----  */
+const myDiscount = 
+[
+    document.getElementById('t01'), 
+    document.getElementById('t02'), 
+    document.getElementById('t03'),
+    document.getElementById('t04')
+];
+
+/*  ----  Article art  ----  */
 let art01;
 
-let prices = [2500, 3850, 5620];
+/*  ----  True selection ----  */
+let itemSelected = true;
 
-console.log('No mames' + articulito[0]);
 
-function addItemDiscount()
+
+
+/*  ---- Add Item to calculate Function  ----  */
+function summArticles()
 {
-  let realPrice = document.getElementById('pInteractivo_01');
-    if(articulito[0].checked)
-    { 
-       realPrice.innerHTML = prices[0] + 'MNX';
+  let showRealPrice = document.getElementById('pInteractivo_01');
+  let grandTotal = 0;  
+  for(i = 0; i < 3; i++)
+  {
+    let articleName = ['Terevikun', 'Kulanina', 'Megakulanina'];
+    console.log(articleName[i]);
+    if(articulito[i].checked == itemSelected)
+    {
+      thisPrice = prices[i];
+      console.log(`Precio del articulo ${thisPrice}`);
+      grandTotal = grandTotal + thisPrice; 
     }
+    else {
+      console.log('No se ha añadido al carrito');
+    }
+  }
+  console.log(`Este es el precio total = ${grandTotal}`);
+  showRealPrice.innerHTML = grandTotal;
 }
 
-
-/*  ----  Dicount Function  ----  */
+/*  ---- Add Discount Function  ----  */
 
 const botoncitoClacular = document.getElementById('calculate');
 botoncitoClacular.addEventListener('click', discounting);
 
-let basePrice = 0;
+let validation = 
+[
+  document.getElementById('keyC01'),
+  document.getElementById('keyC02'),
+  document.getElementById('keyC03'),
+  document.getElementById('keyC04'),
+];
 
 function discounting()
 {
-   for(i = 0; i < 3; i++)
-   {
-       if(articulito[i].checked)
-       {
-           basePrice += prices[i];
-       }
-    console.log('Este es el precio base' + basePrice);
-   }
-
-   let botoncito01 = document.getElementById('t01');
-   console.log('Mi botoncito funciona :D');
-   if(botoncito01.checked)
-   {
-       console.log('Mi input de descuento fucniona :3');
-       discount = availableDiscounts[0];
-       const validation01 = document.getElementById('keyC01');
-       console.log(discount);
-       if(ticket_01 == validation01.value)
-       {
-            console.log('Descuento ingresado correctamente');
-            let finalPrice = (basePrice * (100 - discount) / 100);
-            let showDiscount = document.getElementById('pInteractivo_02');
-            showDiscount.innerHTML = '!Este es tu precio final!' + finalPrice;
-       }
-           else 
-           {
-           alert('Tu código es incorrecto perra >:v');
-           }
-   }
-   else 
-   {
-       alert('No has seleccionado ningún descuento');
-   }    
+  let basePrice = document.getElementById('pInteractivo_01');
+  console.log('Mi botoncito funciona :D');
+  console.log(basePrice.textContent);
+  let priceTo = parseFloat(basePrice.textContent);
+  console.log(priceTo);
+  let maxDiscount = 0;
+/*  ---- Loop for get discounts  ----  */
+  for(i = 0; i < availableDiscounts.length; i++)
+  {
+    if(myDiscount[i].checked == itemSelected)
+     {
+        console.log('Mi input de descuento fucniona :3');
+        discount = availableDiscounts[i];
+ 
+/*  ----  Validation Discount Function  ----  */      
+        if(ticket[i] == validation[i].value)
+        {
+        console.log(`Este es el descuento actual ${discount}`);    
+        console.log('Descuento ingresado correctamente');
+        maxDiscount = maxDiscount + availableDiscounts[i];
+        console.log(`Este es el descuento acumulado ${maxDiscount}`);
+        let finalPrice = priceTo * (100 - maxDiscount) / 100;
+        let showDiscount = document.getElementById('pInteractivo_02');
+        showDiscount.innerHTML = finalPrice;
+        }
+        else 
+        {
+          alert('Tu código es incorrecto amigo >:v');
+          return;
+        }   
+    }
+    else 
+    {
+      alert('No has seleccionado ningún descuento');
+      return;
+    }    
+  }
 }
-
